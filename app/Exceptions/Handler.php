745b07c158a1b98c78dd8f4d4f7d96950b2dc8b2
Kevin MITERMITE <40229021+mitermitek@users.noreply.github.com>
2023-09-25 19:41:01 +0200

    Update handler exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,9 +2,18 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Http\Request;
+use Illuminate\Session\TokenMismatchException;
+use Illuminate\Validation\UnauthorizedException;
+use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -42,6 +51,61 @@ class Handler extends ExceptionHandler
     public function render($request, Throwable $e): Response
     {
         if ($request->is('api/*')) {
+            if ($e instanceof ValidationException) {
+                return response()->json([
+                    'message' => 'Erreur de validation',
+                    'errors' => $e->errors(),
+                ], 422);
+            }
+
+            if ($e instanceof AuthenticationException) {
+                return response()->json([
+                    'message' => 'Pas authentifié',
+                ], 401);
+            }
+
+            if ($e instanceof ModelNotFoundException) {
+                return response()->json([
+                    'message' => 'Ressource non trouvée',
+                ], 404);
+            }
+
+            if ($e instanceof NotFoundHttpException) {
+                return response()->json([
+                    'message' => 'Route non trouvée',
+                ], 404);
+            }
+
+            if ($e instanceof MethodNotAllowedHttpException) {
+                return response()->json([
+                    'message' => 'Méthode non autorisée',
+                ], 405);
+            }
+
+            if ($e instanceof QueryException) {
+                return response()->json([
+                    'message' => 'Erreur de base de données',
+                ], 500);
+            }
+
+            if ($e instanceof AuthorizationException) {
+                return response()->json([
+                    'message' => 'Pas autorisé',
+                ], 403);
+            }
+
+            if ($e instanceof TokenMismatchException) {
+                return response()->json([
+                    'message' => 'Token invalide',
+                ], 419);
+            }
+
+            if ($e instanceof UnauthorizedException) {
+                return response()->json([
+                    'message' => 'Pas autorisé',
+                ], 401);
+            }
+
             return response()->json([
                 'message' => $e->getMessage(),
                 'code' => $e->getCode(),
