d2e665abd15762253bdc247f0d346177c9179c21
Kevin MITERMITE <40229021+mitermitek@users.noreply.github.com>
2023-10-16 16:20:18 +0200

    Update user request

--- app/Http/Requests/API/User/UserRequest.php
+++ app/Http/Requests/API/User/UserRequest.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Requests\API\User;
 
+use App\Enums\StatusEnum;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Enum;
 
 class UserRequest extends FormRequest
 {
@@ -39,6 +41,9 @@ class UserRequest extends FormRequest
                 Rule::unique('users', 'email')->ignore($this->user?->id)
             ],
             'password' => (!$this->user?->id || $this->password) ? ['required', 'string', 'min:8'] : ['nullable'],
+            'is_super_admin' => ['sometimes', 'boolean'],
+            'teacher_status' => ['sometimes', 'string', new Enum(StatusEnum::class)],
+            'learner_mode' => ['sometimes', 'exists:modes,id'],
         ];
     }
 
@@ -66,7 +71,11 @@ class UserRequest extends FormRequest
             'email.unique' => 'L\'adresse mail est déjà utilisée.',
             'password.required' => 'Le mot de passe est obligatoire.',
             'password.string' => 'Le mot de passe doit être une chaîne de caractères.',
-            'password.min' => 'Le mot de passe doit contenir au moins 8 caractères.'
+            'password.min' => 'Le mot de passe doit contenir au moins 8 caractères.',
+            'is_super_admin.boolean' => 'Le champ super administrateur doit être un booléen.',
+            'teacher_status.string' => 'Le statut du formateur doit être une chaîne de caractères.',
+            'teacher_status.enum' => 'Le statut du formateur doit être un des statuts suivants : ' . implode(', ', collect(StatusEnum::cases())->pluck('value')->toArray()),
+            'learner_mode.exists' => 'Le mode de l\'apprenant doit être un mode existant.',
         ];
     }
 }
