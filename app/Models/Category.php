7df06f7eda172dbc541fdd59afce7e73c0a8525d
Kevin MITERMITE <40229021+mitermitek@users.noreply.github.com>
2023-10-16 21:24:23 +0200

    Add category management

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,6 +8,7 @@ namespace App\Models;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
@@ -25,6 +26,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
  */
 class Category extends Model
 {
+    use HasFactory;
+
     protected $table = 'categories';
 
     protected $fillable = [
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ModeSeeder::class,
             UserSeeder::class,
+            CategorySeeder::class,
         ]);
     }
 }
