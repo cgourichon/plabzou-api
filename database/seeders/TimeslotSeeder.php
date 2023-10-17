e32feb8485909fad02a113a9293314216ac03c18
Kevin MITERMITE <40229021+mitermitek@users.noreply.github.com>
2023-10-17 19:11:41 +0200

    Update timeslot seeder

--- database/seeders/TimeslotSeeder.php
+++ database/seeders/TimeslotSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Learner;
+use App\Models\Teacher;
 use App\Models\Timeslot;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,12 @@ class TimeslotSeeder extends Seeder
      */
     public function run(): void
     {
-        Timeslot::factory()->count(100)->create();
+        Timeslot::factory()->count(100)->create()->each(function (Timeslot $timeslot) {
+            $teachers = Teacher::inRandomOrder()->limit(rand(1, 5))->get();
+            $timeslot->teachers()->attach($teachers);
+
+            $learners = Learner::inRandomOrder()->limit(rand(1, 10))->get();
+            $timeslot->learners()->attach($learners);
+        });
     }
 }
