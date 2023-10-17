2d83386d406314d15a902e469b466cb30ead0d5b
Kevin MITERMITE <40229021+mitermitek@users.noreply.github.com>
2023-10-17 21:38:09 +0200

    Update timeslot request

--- app/Http/Requests/API/Timeslot/TimeslotRequest.php
+++ app/Http/Requests/API/Timeslot/TimeslotRequest.php
@@ -27,7 +27,13 @@ class TimeslotRequest extends FormRequest
             'room' => ['required', 'integer', 'exists:rooms,id'],
             'starts_at' => ['required', 'date'],
             'ends_at' => ['required', 'date'],
-            'is_validated' => ['nullable', 'boolean']
+            'is_validated' => ['nullable', 'boolean'],
+            'learners' => ['required', 'array'],
+            'learners.*.id' => ['required', 'exists:users,id'],
+            'learners.*.learner.id' => ['required', 'exists:learners,id'],
+            'teachers' => ['required', 'array'],
+            'teachers.*.id' => ['required', 'exists:users,id'],
+            'teachers.*.teacher.id' => ['required', 'exists:teachers,id'],
         ];
     }
 
@@ -49,7 +55,19 @@ class TimeslotRequest extends FormRequest
             'starts_at.date' => 'La date de début doit être une date.',
             'ends_at.required' => 'La date de fin est obligatoire.',
             'ends_at.date' => 'La date de fin doit être une date.',
-            'is_validated.boolean' => 'La validation doit être un booléen.'
+            'is_validated.boolean' => 'La validation doit être un booléen.',
+            'learners.required' => 'Les apprenants sont obligatoires.',
+            'learners.array' => 'Les apprenants doivent être un tableau.',
+            'learners.*.id.required' => 'L\'apprenant est obligatoire.',
+            'learners.*.id.exists' => 'L\'apprenant n\'existe pas.',
+            'learners.*.learner.id.required' => 'L\'apprenant est obligatoire.',
+            'learners.*.learner.id.exists' => 'L\'apprenant n\'existe pas.',
+            'teachers.required' => 'Les formateurs sont obligatoires.',
+            'teachers.array' => 'Les formateurs doivent être un tableau.',
+            'teachers.*.id.required' => 'Le formateur est obligatoire.',
+            'teachers.*.id.exists' => 'Le formateur n\'existe pas.',
+            'teachers.*.teacher.id.required' => 'Le formateur est obligatoire.',
+            'teachers.*.teacher.id.exists' => 'Le formateur n\'existe pas.',
         ];
     }
 }
