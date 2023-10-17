0f5541f40af8038f256539cdb082e38c1bcd39f8
Kevin MITERMITE <40229021+mitermitek@users.noreply.github.com>
2023-10-17 12:39:01 +0200

    Add room seeder

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -8,6 +8,7 @@ namespace App\Models;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -30,7 +31,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  */
 class Room extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, HasFactory;
 
     protected $table = 'rooms';
 
