5854a3e36ca543d1380d694ebce96f35716613bb
Arthur Mallet <malleta01@gmail.com>
2023-10-18 11:16:01 +0200

    Modif' pour liaison basique entre formateur et formation

--- database/migrations/2023_08_25_153302_create_teacher_training_table.php
+++ database/migrations/2023_08_25_153302_create_teacher_training_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
             $table->id();
             $table->foreignId('teacher_id')->constrained('teachers', 'user_id');
             $table->foreignId('training_id')->constrained();
-            $table->date('latest_upgrade_date');
+            $table->date('latest_upgrade_date')->nullable();
             $table->boolean('is_active')->default(true);
             $table->string('reason')->nullable();
             $table->timestamps();
