e313ad0b894d2ae9b440a6d61b8a4ede7b7aa4c9
corentinho <corentin.gourichon@proton.me>
2023-10-19 10:40:23 +0200

    PBZ-83 Ajouter les formations dans le cursus

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -8,8 +8,10 @@ namespace App\Models;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
@@ -27,7 +29,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  */
 class Course extends Model
 {
-    use SoftDeletes;
+    use HasFactory, SoftDeletes;
 
     protected $table = 'courses';
 
@@ -35,16 +37,14 @@ class Course extends Model
         'name'
     ];
 
-    public function promotions(): BelongsToMany
+    public function promotions(): HasMany
     {
-        return $this->belongsToMany(Promotion::class)
-            ->withPivot('id')
-            ->withTimestamps();
+        return $this->hasMany(Promotion::class);
     }
 
     public function trainings(): BelongsToMany
     {
-        return $this->belongsToMany(Training::class, 'training_course')
+        return $this->belongsToMany(Training::class, 'training_course', 'course_id', 'training_id')
             ->withPivot('id')
             ->withTimestamps();
     }
