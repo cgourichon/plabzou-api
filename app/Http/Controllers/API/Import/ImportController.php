fc732caecae3e4678dd1f8419e7c7388eeb4cd4b
Marjo <marjorie.gestin2020@campus-eni.fr>
2023-10-21 11:39:12 +0200

    update controller

--- app/Http/Controllers/API/Import/ImportController.php
+++ app/Http/Controllers/API/Import/ImportController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\API\Import;
 use App\Http\Controllers\API\BaseController;
 use App\Http\Requests\API\Import\ImportRequest;
 use App\Imports\AdministrativeEmployeesImport;
-use App\Imports\CoursesImport;
 use App\Imports\LearnersImport;
 use App\Imports\PromotionsImport;
 use App\Imports\RoomsImport;
@@ -29,50 +28,51 @@ class ImportController extends BaseController
 
     public function importTeachers(ImportRequest $request): JsonResponse
     {
-        /*try {*/
-        Excel::import(new TeachersImport, $request->file('import_file'));
-        return $this->success([], "L'import des formateurs a réussi");
-    } /*catch (\Exception $e) {
+        try {
+            Excel::import(new TeachersImport, $request->file('import_file'));
+            return $this->success([], "L'import des formateurs a réussi");
+        } catch (\Exception $e) {
             return $this->error("Erreur lors de l'import, verifiez le contenu du fichiers");
         }
-    }*/
+    }
     public function importAdministrativeEmployees(ImportRequest $request): JsonResponse
     {
-        /*try {*/
-        Excel::import(new AdministrativeEmployeesImport, $request->file('import_file'));
-        return $this->success([], "L'import des employés administratifs a réussi");
-    } /*catch (\Exception $e) {
+        try {
+            Excel::import(new AdministrativeEmployeesImport, $request->file('import_file'));
+            return $this->success([], "L'import des employés administratifs a réussi");
+        } catch (\Exception $e) {
             return $this->error("Erreur lors de l'import, verifiez le contenu du fichiers");
         }
-    }*/
+    }
 
     public function importRooms(ImportRequest $request): JsonResponse
     {
-        /*try {*/
-        Excel::import(new RoomsImport, $request->file('import_file'));
-        return $this->success([], "L'import des salles a réussi");
-    } /*catch (\Exception $e) {
+        try {
+            Excel::import(new RoomsImport, $request->file('import_file'));
+            return $this->success([], "L'import des salles a réussi");
+        } catch (\Exception $e) {
             return $this->error("Erreur lors de l'import, verifiez le contenu du fichiers");
         }
-    }*/
+    }
 
     public function importPromotions(ImportRequest $request): JsonResponse
     {
-        /*try {*/
-        Excel::import(new PromotionsImport(), $request->file('import_file'));
-        return $this->success([], "L'import des promotions a réussi");
-    } /*catch (\Exception $e) {
+        try {
+            Excel::import(new PromotionsImport(), $request->file('import_file'));
+            return $this->success([], "L'import des promotions a réussi");
+        } catch (\Exception $e) {
             return $this->error("Erreur lors de l'import, verifiez le contenu du fichiers");
         }
-    }*/
+    }
+
     public function importLearners(ImportRequest $request): JsonResponse
     {
-        /*try {*/
-        Excel::import(new LearnersImport, $request->file('import_file'));
-        return $this->success([], "L'import des apprenants a réussi");
-    } /*catch (\Exception $e) {
+        try {
+            Excel::import(new LearnersImport, $request->file('import_file'));
+            return $this->success([], "L'import des apprenants a réussi");
+        } catch (\Exception $e) {
             return $this->error("Erreur lors de l'import, verifiez le contenu du fichiers");
         }
-    }*/
+    }
 
 }
